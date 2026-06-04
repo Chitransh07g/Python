0984151967c0dee3a27dd0bb048db8386b061eff
Veyrixx <chitransh.k.g.001@gmail.com>
2026-06-04 20:11:52 +0530

    first commit

---
+++ Pyhton Learning/Learning IF Statements/cars.py
@@ -0,0 +1,13 @@
+cars = ['BMW','audi','ferari','hundai']
+for car in cars:
+    if car.lower() =='bmw':
+        print(car.upper())
+    else:
+        print (car.title())
+
+age = 15
+if age >=15 or age <=25:
+    print ('you are correct')
+
+print ('hacc' in cars)
+# usinf " in " and "not in " 
