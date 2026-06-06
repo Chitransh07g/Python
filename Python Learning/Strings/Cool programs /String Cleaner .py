0ab17be810cd5c50632fe1a21ed2b9fa8c202a94
Chitransh07g <chitransh.k.g.001@gmail.com>
2026-06-06 18:34:25 +0530

    String Challenge 1: String Cleaner

---
+++ Python Learning/Strings/Cool programs /String Cleaner .py
@@ -0,0 +1,4 @@
+text = "   Hello World   "
+new = text.strip()
+print(f"After removing the spaces the string looks like :-{new} \nString in lower case :-{new.lower()} ")
+print(f"Numbers of letter 'l' present int he string are :- {new.lower().count('l')}")
