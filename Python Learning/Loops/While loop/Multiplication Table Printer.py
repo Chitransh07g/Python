5ddcb147fd72551ef958da2ae91bf44bd963e1da
Chitransh07g <chitransh.k.g.001@gmail.com>
2026-06-08 10:41:54 +0530

    feat: implement multiplication table

---
+++ Python Learning/Loops/While loop/Multiplication Table Printer.py
@@ -0,0 +1,9 @@
+num = int(input("Guide me the number for whuch you want Multiplication Table "))
+x = 1
+
+print(f"The Multiplication Table of {num} \n")
+while x <= 10:
+    print(f"{num} x {x} = {num * x}")
+    x += 1
+print("Done!")
+    
