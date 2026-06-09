0d123cbed48ec8833e3a52f2884e4412949c0518
Chitransh07g <chitransh.k.g.001@gmail.com>
2026-06-09 10:26:23 +0530

    removed unwanted file

--- Python Learning/Loops/While loop/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-number = int(input("Please guide me the number upto which you want to me to print Even numbers \n"))
-x = 0
-print ("All the even numbers are ")
-while x <= number:
-    if x % 2 == 0:
-        print(x)
-    x += 1
-
-print("Done")
