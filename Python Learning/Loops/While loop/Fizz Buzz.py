8b3f11a2d17f249dda552255e8c72623d03d58cf
Chitransh07g <chitransh.k.g.001@gmail.com>
2026-06-09 11:04:19 +0530

    fix : the printing part

--- Python Learning/Loops/While loop/Fizz Buzz.py
+++ Python Learning/Loops/While loop/Fizz Buzz.py
@@ -3,15 +3,18 @@ n = 1
 
 print("Lets go ")
 while n <= num :
+
     if n % 3 == 0 and n % 5 == 0:
         print("FizzBuzz")
+
     elif  n % 3 == 0 :
         print("Fizz")
+
     elif n % 5 == 0 :
         print("Buzz")
+
     else :
         print(n)
     n += 1
 
-print("Done!")
-
+print("Done ! ") 
