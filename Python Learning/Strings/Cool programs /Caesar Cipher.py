1ebcf4356f8ee178e2f9f3373ff446919a730596
Chitransh07g <chitransh.k.g.001@gmail.com>
2026-06-15 22:57:26 +0530

    Refactor

--- Python Learning/Strings/Cool programs /Caesar Cipher.py
+++ Python Learning/Strings/Cool programs /Caesar Cipher.py
@@ -1,7 +1,9 @@
 word = "hello"
 char =""
+
 for x in range(len(word)):
    char = char + chr(ord(word[x])+3)
    # here ord() gives the ASCII number of a character
    # chr() converts a number back to a character 
+
 print(char)
