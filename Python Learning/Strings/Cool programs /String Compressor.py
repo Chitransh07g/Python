788a748e7671d9c41a872e897b41476e9c20517b
Chitransh07g <chitransh.k.g.001@gmail.com>
2026-06-15 22:59:33 +0530

    Refactor

--- Python Learning/Strings/Cool programs /String Compressor.py
+++ Python Learning/Strings/Cool programs /String Compressor.py
@@ -1,6 +1,7 @@
 strings = "aaabbbccddddee"
 count = 1
 new = ""
+
 for x in range(1 , len(strings)):
     if strings[x]  == strings[x-1]:
         count += 1
@@ -8,4 +9,5 @@ for x in range(1 , len(strings)):
         new += strings[x-1] + str(count)
         count = 1        
 new += strings[-1] + str(count)
+
 print(new)
