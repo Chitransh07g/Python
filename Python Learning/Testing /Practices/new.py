8499a0757a8792bc2ad9ecfd47ac52dee7be7c0b
Chitransh07g <chitransh.k.g.001@gmail.com>
2026-06-26 22:20:53 +0530

    Practice

---
+++ Python Learning/Testing /Practices/new.py
@@ -0,0 +1,11 @@
+def f1():
+    num1 =int(input("Enter number 1:"))
+    num2 =int(input("Enter number 2:"))
+    num3 =int(input("Enter number 3:"))
+    sum=num1+num2+num3
+    print("Sum is :",sum)
+    print(f"hello{num1}")
+    print("hello")
+
+
+f1()
