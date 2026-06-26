9208f791b69998ad8a226eedcce461cc820964d6
Chitransh07g <chitransh.k.g.001@gmail.com>
2026-06-26 22:23:45 +0530

    Practice

---
+++ Python Learning/Testing /Practices/is.py
@@ -0,0 +1,10 @@
+name = input("Enter your name ")
+initials = ''
+
+for ch in name.title():
+    if ch.isupper():
+        initials += ch+"."
+
+
+print(f"The Initials of your Name is :-{initials.strip()}")  
+print("ALL done")      
