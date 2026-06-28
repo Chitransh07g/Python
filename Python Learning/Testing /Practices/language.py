9bbe7914e83b6be22ff799c2b6f22c272d671734
Chitransh07g <chitransh.k.g.001@gmail.com>
2026-06-28 12:59:58 +0530

    Practice

---
+++ Python Learning/Testing /Practices/language.py
@@ -0,0 +1,9 @@
+languages = {
+    'jen' : 'python',
+    'phill'  :  'c' ,
+    'sarah'  :  'ruby' ,
+    'edward'  :  'python' 
+}
+
+for name , lang in languages.items():
+    print(f"{name.title()}'s favorite language is {lang.title()}")
