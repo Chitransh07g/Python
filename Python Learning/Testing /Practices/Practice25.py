514ca97c520ced712da799f40c709faa348e3c2c
Chitransh07g <chitransh.k.g.001@gmail.com>
2026-07-01 20:41:47 +0530

    Practice

---
+++ Python Learning/Testing /Practices/Practice25.py
@@ -0,0 +1,9 @@
+launguage = {
+    'jen' : 'python' ,
+    'sarah' : 'c' ,
+    'edward' : 'ruby' ,
+    'phill' : 'python' 
+}
+
+for name in sorted(launguage.keys()):
+    print(f"{name.title()} thankyou for taking the poll .")
