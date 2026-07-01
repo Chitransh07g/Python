62ad5b082564dd2f4753e74ac6bdd07f80e7a98d
Chitransh07g <chitransh.k.g.001@gmail.com>
2026-07-01 20:18:46 +0530

    Practice

---
+++ Python Learning/Testing /Practices/Practice23.py
@@ -0,0 +1,12 @@
+launguage = {
+    'jen' : 'python' ,
+    'sarah' : 'c' ,
+    'edward' : 'ruby' ,
+    'phill' : 'python' 
+}
+for name in launguage.keys():
+    print(name.title())
+
+for lang in launguage.values():
+    print(lang.title())
+    
