7e74673a53e7230d8ae18e0d3e6c58194c270870
Chitransh07g <chitransh.k.g.001@gmail.com>
2026-07-04 12:38:59 +0530

    refactor : styles

--- Python Learning/Dictionaries/Word Counter.py
+++ Python Learning/Dictionaries/Word Counter.py
@@ -1,6 +1,8 @@
 sent = input("\nEnter the sentence \n").split()
 temp = {}
 
+# this loop is for counting frequncy of all the words and storing it into a Dictionary
+
 for x in range(len(sent)):
     count = 0
     for y in range(x , len(sent)):
@@ -14,11 +16,17 @@ print("\t\tHere it starts 🖊️")
 print("================================================")
 print("\nHere are all the words with their Frequency\n")
 
+# It prints all the dict pair one by one 
+
+n = 1 
 for key in temp.keys():
-    print(f"\t{key} : {temp[key]}")  
+    print(f"\t{n} → {key} : {temp[key]}")  
+    n += 1
 
 print("\n------------------------------------------------")
 print("\nThe word / words with maximum Frequency  \n")
+
+# Here ,  this finds the word or words with maxmimum frequency and displays it
 
 maximum = max(temp.values())
 ok = {}
@@ -28,8 +36,10 @@ for key in temp.keys():
     if a == maximum:
         ok[b] = a
 
+n = 1
 for key in ok.keys():
-    print(f"\t{key} : {ok[key]}")
+    print(f"\t{n} → {key} : {ok[key]}")
+    n += 1
 
 print("\n================================================")
 print("\t \t Here it ends 😭")
