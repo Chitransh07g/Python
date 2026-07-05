91050e098a7e3b37f0709858c83ceab349dc1cc4
Chitransh07g <chitransh.k.g.001@gmail.com>
2026-07-05 20:37:23 +0530

    Practice

---
+++ Python Learning/Dictionaries/Alovera.py
@@ -0,0 +1,8 @@
+user = {
+    'uesername' : 'efermi' ,
+    'first' : 'enrico' , 
+    'last' : 'fermi'
+}
+print("Hello now it starts ")
+for key , value in user.items():
+    print(f"\nKey : {key} \nValue : {value}")
