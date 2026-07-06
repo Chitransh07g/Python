3656feab448bb73f41341be7ffd46012f2d41861
Chitransh07g <chitransh.k.g.001@gmail.com>
2026-07-06 15:33:31 +0530

    feat : Shirt Maker

---
+++ Python Learning/Functions/Shirt Maker.py
@@ -0,0 +1,6 @@
+def make_shirt(size,text="I Love Python"):
+    print(f"Making a size {size} shirt with the text: {text}")
+
+make_shirt("short",'please make the shirt')
+make_shirt('Medium')
+make_shirt("large","")    
