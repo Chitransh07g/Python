64c8b72186c21d3ec3976afdefe33625c0d51246
Chitransh07g <chitransh.k.g.001@gmail.com>
2026-09-23 17:06:54 +0530

    Feat : learning part 1

---
+++ Python Learning/Classes/dog.py
@@ -0,0 +1,21 @@
+class Dog():
+
+    def __init__(self, name, age):
+        self.name = name 
+        self.age = age
+
+    def sit(self):
+        print(f"{self.name.title()} is now sitting." )    
+
+    def roll_over(self):
+        print(f"{self.name.title()} rolled over!")
+
+
+my_dog = Dog("tommy", 3)
+my_dog = Dog("tommy", 3)
+
+my_dog.sit()
+my_dog.roll_over()
+
+print(f"My dog's name is {my_dog.name.title()}.")  
+print(f"My dog is {my_dog.age} years old.")
